a07c51a10aaab529b4a45bf927115372cd114efe
Abrhammo <abrhammolla4@gmail.com>
2026-01-18 19:55:17 +0300

    create dbt mart models for star schema

---
+++ medical_warehouse/models/marts/fct_messages.sql
@@ -0,0 +1,14 @@
+SELECT
+    s.message_id,
+    c.channel_key,
+    d.date_key,
+    s.message_text,
+    s.message_length,
+    s.view_count,
+    s.forward_count,
+    s.has_image
+FROM {{ ref('stg_telegram_messages') }} s
+JOIN {{ ref('dim_channels') }} c
+  ON s.channel_name = c.channel_name
+JOIN {{ ref('dim_dates') }} d
+  ON DATE(s.message_date) = d.full_date
